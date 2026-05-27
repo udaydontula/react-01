f8f0a66acf524244342459c1ecdeb59c69402e68
udaydontula <touday.dontula@gmail.com>
2026-05-27 21:30:53 +0530

    random fun commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
+import reactImg from './assets/react-core-concepts.png';
+
+const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
+
+function genRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+
 function Header() {
+
+  const description = reactDescriptions[genRandomInt(reactDescriptions.length - 1)];
+
   return (
     <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          Fundamental React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
+      <img src={reactImg} alt="Stylized atom" />
+      <h1>React Essentials</h1>
+      <p>
+        {description} React concepts you will need for almost any app you are going to build!
+      </p>
+    </header>
   );
 }
 
